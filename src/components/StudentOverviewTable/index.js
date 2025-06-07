a85b8db6730a4b443ff98cac0dee5dd9b127bc3c
Leo <liyung.chen@tum.de>
2025-06-07 13:06:53 +0200

    fix Documents columns

--- src/components/StudentOverviewTable/index.js
+++ src/components/StudentOverviewTable/index.js
@@ -47,228 +47,218 @@ import DEMO from '../../store/constant';
 import { green, grey } from '@mui/material/colors';
 import { useTranslation } from 'react-i18next';
 
-const StudentOverviewTable = ({ students }) => {
-    const { t } = useTranslation();
-    const tranform = (stds) => {
-        const transformedStudents = [];
-        if (!stds) {
-            return [];
+const tranform = (stds) => {
+    const transformedStudents = [];
+    if (!stds) {
+        return [];
+    }
+
+    for (const student of stds) {
+        let keys = Object.keys(ProfileNameType);
+        let object_init = {};
+        for (let i = 0; i < keys.length; i++) {
+            object_init[keys[i]] = DocumentStatusType.Missing;
         }
 
-        for (const student of stds) {
-            let keys = Object.keys(ProfileNameType);
-            let object_init = {};
-            for (let i = 0; i < keys.length; i++) {
-                object_init[keys[i]] = DocumentStatusType.Missing;
-            }
-
-            if (student.profile) {
-                student.profile.forEach((item) => {
-                    object_init[item.name] =
-                        DocumentStatusType[item.status] || '';
-                });
-            }
-            let isMissingBaseDocs = false;
-            let total_base_docs_needed = 0;
-            let total_accepted_base_docs_needed = 0;
-            for (let i = 0; i < keys.length; i += 1) {
-                if (object_init[keys[i]] !== DocumentStatusType.NotNeeded) {
-                    total_base_docs_needed += 1;
-                }
-                if (
-                    object_init[keys[i]] === DocumentStatusType.Accepted &&
-                    object_init[keys[i]] !== DocumentStatusType.NotNeeded
-                ) {
-                    total_accepted_base_docs_needed += 1;
-                }
-            }
-            isMissingBaseDocs =
-                total_base_docs_needed > total_accepted_base_docs_needed
-                    ? true
-                    : false;
-            // TODO: logic improvement (necessary field)
-            let isEnglishPassed = check_english_language_passed(
-                student.academic_background
-            );
-
-            let isGermanPassed = check_german_language_passed(
-                student.academic_background
-            );
-
-            let isSurveyCompleted = check_academic_background_filled(
-                student.academic_background
-            );
-
-            let num_apps_decided = numApplicationsDecided(student);
-            let num_apps_closed = numApplicationsSubmitted(student);
-            let areProgramsAllDecided =
-                areProgramsDecidedMoreThanContract(student);
-
-            let is_All_Applications_Submitted =
-                check_all_decided_applications_submitted(student);
-
-            const isCVFinished_b = isCVFinished(student);
-            const isCVAssigned = is_cv_assigned(student);
-
-            var is_uni_assist_needed = check_student_needs_uni_assist(student);
-            let isall_uni_assist_vpd_uploaded =
-                is_all_uni_assist_vpd_uploaded(student);
-
-            let numb_uni_assist_vpd_needed = num_uni_assist_vpd_needed(student);
-            let numb_uni_assist_vpd_uploaded =
-                num_uni_assist_vpd_uploaded(student);
-            let target_degree =
-                student.application_preference?.target_degree || '';
-            let expected_application_year =
-                student.application_preference?.expected_application_date || '';
-            let expected_application_semster =
-                student.application_preference?.expected_application_semester ||
-                '';
-
-            transformedStudents.push({
-                ...prepTaskStudent(student),
-                applying_program_count: student.applying_program_count,
-                year_semester: `${expected_application_year || 'TBD'}/ ${
-                    expected_application_semster || 'TBD'
-                }`,
-                id: student._id.toString(),
-                student,
-                attributesJoined: student.attributes
-                    ?.map((attribute) => attribute.name)
-                    .join(' '),
-                target_degree,
-                isGraduated:
-                    student.academic_background?.university
-                        ?.high_school_isGraduated === 'Yes'
-                        ? student.academic_background?.university
-                              ?.isGraduated === 'Yes'
-                            ? 'Yes'
-                            : 'No'
-                        : 'No',
-                academic_background: student.academic_background,
-                isMissingBaseDocs,
-                CV: !isCVFinished_b
-                    ? isCVAssigned
-                        ? 'In Progress'
-                        : 'Not Created'
-                    : 'Done',
-                ML: `${
-                    student.applications?.filter((application) =>
-                        application.doc_modification_thread?.some(
-                            (thread) =>
-                                isProgramDecided(application) &&
-                                thread.doc_thread_id?.isFinalVersion &&
-                                thread.doc_thread_id?.file_type === 'ML'
-                        )
-                    ).length
-                }/${
-                    student.applications.filter((application) =>
-                        application.doc_modification_thread?.some(
-                            (thread) =>
-                                isProgramDecided(application) &&
-                                thread.doc_thread_id?.file_type === 'ML'
-                        )
-                    ).length
-                }`,
-                RL: `${
-                    student.applications.filter((application) =>
-                        application.doc_modification_thread?.some(
-                            (thread) =>
-                                isProgramDecided(application) &&
-                                thread.doc_thread_id?.isFinalVersion &&
-                                (thread.doc_thread_id?.file_type?.includes(
-                                    'RL'
-                                ) ||
-                                    thread.doc_thread_id?.file_type?.includes(
-                                        'Recommendation'
-                                    ))
-                        )
-                    ).length
-                }/${
-                    student.applications.filter((application) =>
-                        application.doc_modification_thread?.some(
-                            (thread) =>
-                                isProgramDecided(application) &&
-                                (thread.doc_thread_id?.file_type?.includes(
-                                    'RL'
-                                ) ||
-                                    thread.doc_thread_id?.file_type?.includes(
-                                        'Recommendation'
-                                    ))
-                        )
-                    ).length
-                }`,
-                Essay: `${
-                    student.applications.filter((application) =>
-                        application.doc_modification_thread?.some(
-                            (thread) =>
-                                isProgramDecided(application) &&
-                                thread.doc_thread_id?.isFinalVersion &&
-                                thread.doc_thread_id?.file_type?.includes(
-                                    'Essay'
-                                )
-                        )
-                    ).length
-                }/${
-                    student.applications.filter((application) =>
-                        application.doc_modification_thread?.some(
-                            (thread) =>
-                                isProgramDecided(application) &&
-                                thread.doc_thread_id?.file_type?.includes(
-                                    'Essay'
-                                )
-                        )
-                    ).length
-                }`,
-                Portals: `${
-                    student.applications?.length === 0
-                        ? '-'
-                        : to_register_application_portals(student)
-                          ? 'Missing'
-                          : 'Complete'
-                }`,
-                uniassist: `${
-                    is_uni_assist_needed
-                        ? isall_uni_assist_vpd_uploaded
-                            ? 'Done'
-                            : `(${numb_uni_assist_vpd_uploaded}/${numb_uni_assist_vpd_needed}) Incomplete `
-                        : '-'
-                }`,
-                total_base_docs_needed,
-                isEnglishPassed,
-                isGermanPassed,
-                program_selection: `${areProgramsAllDecided ? 'O' : '-'}${num_apps_decided}/${student.applying_program_count}`,
-                application: `${
-                    is_All_Applications_Submitted &&
-                    num_apps_closed >= student.applying_program_count
-                        ? 'O'
-                        : '-'
-                }${num_apps_closed}/${student.applying_program_count}`,
-                nextProgram: getNextProgramName(student),
-                nextProgramDeadline: getNextProgramDeadline(student),
-                nextProgramDayleft: getNextProgramDayleft(student),
-                nextProgramStatus: getNextProgramStatus(student),
-                survey: isSurveyCompleted ? 'Yes' : 'No',
-                basedocument: `${total_accepted_base_docs_needed}/${total_base_docs_needed}`,
-                total_accepted_base_docs_needed,
-                courseAnalysis: needUpdateCourseSelection(student),
-                num_apps_decided,
-                num_apps_closed,
-                areProgramsAllDecided,
-                is_All_Applications_Submitted,
-                isCVFinished_b,
-                isCVAssigned,
-                is_uni_assist_needed,
-                isall_uni_assist_vpd_uploaded,
-                numb_uni_assist_vpd_needed,
-                numb_uni_assist_vpd_uploaded,
-                expected_application_year,
-                expected_application_semster
+        if (student.profile) {
+            student.profile.forEach((item) => {
+                object_init[item.name] = item.status || '';
             });
         }
+        let isMissingBaseDocs = false;
+        let total_base_docs_needed = 0;
+        let total_accepted_base_docs_needed = 0;
+        for (let i = 0; i < keys.length; i += 1) {
+            if (object_init[keys[i]] !== DocumentStatusType.NotNeeded) {
+                total_base_docs_needed += 1;
+            }
+            if (
+                object_init[keys[i]] === DocumentStatusType.Accepted &&
+                object_init[keys[i]] !== DocumentStatusType.NotNeeded
+            ) {
+                console.log(keys[i]);
+                total_accepted_base_docs_needed += 1;
+            }
+        }
+        isMissingBaseDocs =
+            total_base_docs_needed > total_accepted_base_docs_needed
+                ? true
+                : false;
+        // TODO: logic improvement (necessary field)
+        let isEnglishPassed = check_english_language_passed(
+            student.academic_background
+        );
 
-        return transformedStudents;
-    };
+        let isGermanPassed = check_german_language_passed(
+            student.academic_background
+        );
+
+        let isSurveyCompleted = check_academic_background_filled(
+            student.academic_background
+        );
+
+        let num_apps_decided = numApplicationsDecided(student);
+        let num_apps_closed = numApplicationsSubmitted(student);
+        let areProgramsAllDecided = areProgramsDecidedMoreThanContract(student);
+
+        let is_All_Applications_Submitted =
+            check_all_decided_applications_submitted(student);
+
+        const isCVFinished_b = isCVFinished(student);
+        const isCVAssigned = is_cv_assigned(student);
+
+        var is_uni_assist_needed = check_student_needs_uni_assist(student);
+        let isall_uni_assist_vpd_uploaded =
+            is_all_uni_assist_vpd_uploaded(student);
+
+        let numb_uni_assist_vpd_needed = num_uni_assist_vpd_needed(student);
+        let numb_uni_assist_vpd_uploaded = num_uni_assist_vpd_uploaded(student);
+        let target_degree = student.application_preference?.target_degree || '';
+        let expected_application_year =
+            student.application_preference?.expected_application_date || '';
+        let expected_application_semster =
+            student.application_preference?.expected_application_semester || '';
+
+        transformedStudents.push({
+            ...prepTaskStudent(student),
+            applying_program_count: student.applying_program_count,
+            year_semester: `${expected_application_year || 'TBD'}/ ${
+                expected_application_semster || 'TBD'
+            }`,
+            id: student._id.toString(),
+            student,
+            attributesJoined: student.attributes
+                ?.map((attribute) => attribute.name)
+                .join(' '),
+            target_degree,
+            isGraduated:
+                student.academic_background?.university
+                    ?.high_school_isGraduated === 'Yes'
+                    ? student.academic_background?.university?.isGraduated ===
+                      'Yes'
+                        ? 'Yes'
+                        : 'No'
+                    : 'No',
+            academic_background: student.academic_background,
+            isMissingBaseDocs,
+            CV: !isCVFinished_b
+                ? isCVAssigned
+                    ? 'In Progress'
+                    : 'Not Created'
+                : 'Done',
+            ML: `${
+                student.applications?.filter((application) =>
+                    application.doc_modification_thread?.some(
+                        (thread) =>
+                            isProgramDecided(application) &&
+                            thread.doc_thread_id?.isFinalVersion &&
+                            thread.doc_thread_id?.file_type === 'ML'
+                    )
+                ).length
+            }/${
+                student.applications.filter((application) =>
+                    application.doc_modification_thread?.some(
+                        (thread) =>
+                            isProgramDecided(application) &&
+                            thread.doc_thread_id?.file_type === 'ML'
+                    )
+                ).length
+            }`,
+            RL: `${
+                student.applications.filter((application) =>
+                    application.doc_modification_thread?.some(
+                        (thread) =>
+                            isProgramDecided(application) &&
+                            thread.doc_thread_id?.isFinalVersion &&
+                            (thread.doc_thread_id?.file_type?.includes('RL') ||
+                                thread.doc_thread_id?.file_type?.includes(
+                                    'Recommendation'
+                                ))
+                    )
+                ).length
+            }/${
+                student.applications.filter((application) =>
+                    application.doc_modification_thread?.some(
+                        (thread) =>
+                            isProgramDecided(application) &&
+                            (thread.doc_thread_id?.file_type?.includes('RL') ||
+                                thread.doc_thread_id?.file_type?.includes(
+                                    'Recommendation'
+                                ))
+                    )
+                ).length
+            }`,
+            Essay: `${
+                student.applications.filter((application) =>
+                    application.doc_modification_thread?.some(
+                        (thread) =>
+                            isProgramDecided(application) &&
+                            thread.doc_thread_id?.isFinalVersion &&
+                            thread.doc_thread_id?.file_type?.includes('Essay')
+                    )
+                ).length
+            }/${
+                student.applications.filter((application) =>
+                    application.doc_modification_thread?.some(
+                        (thread) =>
+                            isProgramDecided(application) &&
+                            thread.doc_thread_id?.file_type?.includes('Essay')
+                    )
+                ).length
+            }`,
+            Portals: `${
+                student.applications?.length === 0
+                    ? '-'
+                    : to_register_application_portals(student)
+                      ? 'Missing'
+                      : 'Complete'
+            }`,
+            uniassist: `${
+                is_uni_assist_needed
+                    ? isall_uni_assist_vpd_uploaded
+                        ? 'Done'
+                        : `(${numb_uni_assist_vpd_uploaded}/${numb_uni_assist_vpd_needed}) Incomplete `
+                    : '-'
+            }`,
+            total_base_docs_needed,
+            isEnglishPassed,
+            isGermanPassed,
+            program_selection: `${areProgramsAllDecided ? 'O' : '-'}${num_apps_decided}/${student.applying_program_count}`,
+            application: `${
+                is_All_Applications_Submitted &&
+                num_apps_closed >= student.applying_program_count
+                    ? 'O'
+                    : '-'
+            }${num_apps_closed}/${student.applying_program_count}`,
+            nextProgram: getNextProgramName(student),
+            nextProgramDeadline: getNextProgramDeadline(student),
+            nextProgramDayleft: getNextProgramDayleft(student),
+            nextProgramStatus: getNextProgramStatus(student),
+            survey: isSurveyCompleted ? 'Yes' : 'No',
+            basedocument: `${total_accepted_base_docs_needed}/${total_base_docs_needed}`,
+            total_accepted_base_docs_needed,
+            courseAnalysis: needUpdateCourseSelection(student),
+            num_apps_decided,
+            num_apps_closed,
+            areProgramsAllDecided,
+            is_All_Applications_Submitted,
+            isCVFinished_b,
+            isCVAssigned,
+            is_uni_assist_needed,
+            isall_uni_assist_vpd_uploaded,
+            numb_uni_assist_vpd_needed,
+            numb_uni_assist_vpd_uploaded,
+            expected_application_year,
+            expected_application_semster
+        });
+    }
+
+    return transformedStudents;
+};
+
+const StudentOverviewTable = ({ students }) => {
+    const { t } = useTranslation();
+
     const memoizedColumns = useMemo(() => {
         return [
             {
