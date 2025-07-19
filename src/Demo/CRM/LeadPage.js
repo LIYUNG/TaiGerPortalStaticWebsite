675433389a4e6404d19c5fb488f97d7c6bf1f5d6
Alexander Jensen <ajexsen@users.noreply.github.com>
2025-07-20 00:02:20 +0200

    Feat: implement lead fetching logic in LeadPage component

--- src/Demo/CRM/LeadPage.js
+++ src/Demo/CRM/LeadPage.js
@@ -1,4 +1,5 @@
 import { Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { Box, Breadcrumbs, Link, Typography } from '@mui/material';
 import i18next from 'i18next';
 
@@ -7,13 +8,29 @@ import DEMO from '../../store/constant';
 import { useAuth } from '../../components/AuthProvider';
 import { appConfig } from '../../config';
 import { is_TaiGer_role } from '@taiger-common/core';
+import { request } from '../../api/request';
 
 const LeadPage = () => {
     const { user } = useAuth();
-
     if (!is_TaiGer_role(user)) {
         return <Navigate to={`${DEMO.DASHBOARD_LINK}`} />;
     }
+
+    // Temporory workaround to fetch transcripts
+    // TODO: implement actual/proper API call to fetch transcripts with UseQuery
+    const [leads, setLeads] = useState([]);
+
+    useEffect(() => {
+        request
+            .get('/api/crm/leads')
+            .then((data) => {
+                setLeads(data?.data?.data || []);
+            })
+            .catch((error) => {
+                console.error('Failed to fetch leads:', error);
+            });
+    }, []);
+
     TabTitle(i18next.t('Leads', { ns: 'common' }));
 
     return (
@@ -39,6 +56,7 @@ const LeadPage = () => {
                     {i18next.t('Leads', { ns: 'common' })}
                 </Typography>
             </Breadcrumbs>
+            <>{JSON.stringify(leads)}</>
         </Box>
     );
 };
