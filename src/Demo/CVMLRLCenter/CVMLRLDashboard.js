3d4884e353c487b1b3db972cc436cd9c559d0db0
Leo <liyung.chen@tum.de>
2025-08-25 13:55:14 +0200

    fix no input task bug

--- src/Demo/CVMLRLCenter/CVMLRLDashboard.js
+++ src/Demo/CVMLRLCenter/CVMLRLDashboard.js
@@ -59,7 +59,7 @@ const CVMLRLDashboard = (props) => {
         (open_task) =>
             open_task.show &&
             !open_task.isFinalVersion &&
-            open_task.latest_message_left_by_id === ''
+            open_task.latest_message_left_by_id === '- None - '
     );
 
     const cvmlrl_closed_v2 = open_tasks_arr.filter(
