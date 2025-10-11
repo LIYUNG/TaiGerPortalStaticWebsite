a16897b618a04fc949b39dcee29d0ccad6cb45a5
Leo <liyung.chen@tum.de>
2025-10-11 22:33:10 +0200

    improve thread edit experience

--- src/components/Message/MessageList.js
+++ src/components/Message/MessageList.js
@@ -1,32 +1,71 @@
-import React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Stack, Typography, Button } from '@mui/material';
 import ChatIcon from '@mui/icons-material/Chat';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import MessageCard from './MessageCard';
 
 const MessageList = (props) => {
     const messageCount = props.thread?.messages?.length || 0;
+    const [visibleMessageCount, setVisibleMessageCount] = useState(2);
 
-    const thread = props.thread?.messages?.map((message, i) => (
-        <MessageCard
-            accordionKeys={props.accordionKeys}
-            apiPrefix={props.apiPrefix}
-            documentsthreadId={props.documentsthreadId}
-            id={message._id}
-            idx={i}
-            isFirst={i === 0}
-            isLast={i === messageCount - 1}
-            isLoaded={props.isLoaded}
-            key={i}
-            message={message}
-            onDeleteSingleMessage={props.onDeleteSingleMessage}
-            singleExpandtHandler={props.singleExpandtHandler}
-            user={props.user}
-        />
-    ));
+    const handleLoadMore = () => {
+        setVisibleMessageCount((prevCount) =>
+            Math.min(prevCount + 5, messageCount)
+        );
+    };
+
+    // Get the last N messages based on visibleMessageCount
+    const messagesToShow =
+        props.thread?.messages?.slice(-visibleMessageCount) || [];
+    const hasMoreMessages = messageCount > visibleMessageCount;
+
+    const thread = messagesToShow.map((message, i) => {
+        const originalIndex = messageCount - visibleMessageCount + i;
+        return (
+            <MessageCard
+                accordionKeys={props.accordionKeys}
+                apiPrefix={props.apiPrefix}
+                documentsthreadId={props.documentsthreadId}
+                id={message._id}
+                idx={originalIndex}
+                isFirst={originalIndex === 0}
+                isLast={originalIndex === messageCount - 1}
+                isLoaded={props.isLoaded}
+                key={message._id}
+                message={message}
+                onDeleteSingleMessage={props.onDeleteSingleMessage}
+                singleExpandtHandler={props.singleExpandtHandler}
+                user={props.user}
+            />
+        );
+    });
 
     return (
         <Box sx={{ mt: 1 }}>
+            {/* Load More Button */}
+            {hasMoreMessages && (
+                <Box sx={{ mb: 2, textAlign: 'center' }}>
+                    <Button
+                        onClick={handleLoadMore}
+                        startIcon={<ExpandMoreIcon />}
+                        sx={{
+                            borderRadius: 2,
+                            textTransform: 'none',
+                            px: 3
+                        }}
+                        variant="outlined"
+                    >
+                        Load {Math.min(5, messageCount - visibleMessageCount)}{' '}
+                        more message
+                        {Math.min(5, messageCount - visibleMessageCount) > 1
+                            ? 's'
+                            : ''}{' '}
+                        ({messageCount - visibleMessageCount} older)
+                    </Button>
+                </Box>
+            )}
+
             {/* Messages Stack */}
             <Stack spacing={1.5}>
                 {messageCount > 0 ? (
