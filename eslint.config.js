0dc4849c4f2294829da5d864ca08def9fd981673
Leo <liyung.chen@tum.de>
2026-07-12 20:45:43 +0200

    init ocmmit

--- eslint.config.js
+++ eslint.config.js
@@ -6,33 +6,41 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist','node_modules']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist', 'node_modules']),
+    {
+        ignores: [
+            'dist/**',
+            'built/**',
+            'node_modules/**',
+            '**/*.js'
+        ]
     },
-  },
-  // Files that intentionally export both components and non-components (contexts, providers, utils)
-  {
-    files: [
-      'src/utils/contants.tsx',
-      'src/contexts/use-snack-bar.tsx',
-      'src/components/AuthProvider/index.tsx',
-      'src/components/SurveyProvider/index.tsx',
-      'src/components/Tabs/index.tsx',
-      'src/components/ThemeProvider/index.tsx',
-      'src/test/test-utils.tsx',
-    ],
-    rules: {
-      'react-refresh/only-export-components': 'off',
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser
+        }
     },
-  },
-])
+    // Files that intentionally export both components and non-components (contexts, providers, utils)
+    {
+        files: [
+            'src/utils/contants.tsx',
+            'src/contexts/use-snack-bar.tsx',
+            'src/components/AuthProvider/index.tsx',
+            'src/components/SurveyProvider/index.tsx',
+            'src/components/Tabs/index.tsx',
+            'src/components/ThemeProvider/index.tsx',
+            'src/test/test-utils.tsx'
+        ],
+        rules: {
+            'react-refresh/only-export-components': 'off'
+        }
+    }
+]);
